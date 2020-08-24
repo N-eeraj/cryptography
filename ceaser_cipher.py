bc2281374b020a537f2e96a85e20be1c0b0f04cf
N-eeraj <neeraj7rajeena@gmail.com>
2020-08-24 11:17:22 +0530

    removed special charecters

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -1,23 +1,31 @@
-from termcolor import colored
+from termcolor import colored #module to change colors in terminal
 
-def hasNum(word):
-	for letter in word:
-		if letter.isnumeric():
-			return True
-def errorMsg(error):
+def errorMsg(error): #to make the error message red
 	print(colored(error, 'red'))
 
+def isLetter(word): #to check if input has only letters & space
+	for letter in word:
+		input_ascii = ord(letter)
+		if  input_ascii== 32:
+			continue
+		if not 64 < input_ascii  < 90:
+			errorMsg(letter + ' is an Invalid input')
+			return True
+
+def inputMsg(msg): #to make input prompt white
+	return (input(colored(msg, 'white')))
+
 while True:
-	txt = input('Enter Text : ').upper()
-	if hasNum(txt):
+	txt = inputMsg('Enter Text : ').upper()
+	if isLetter(txt):
 		errorMsg('Error! Should have only letters\n')
 	else:
 		break
 
 while True:
 	try:
-		key = int(input('Enter Key : '))
-		if not 0<key<26:
+		key = int(inputMsg('Enter Key : '))
+		if not 0 < key < 26:
 			errorMsg('Error! Invalid Key\n')
 			continue
 		break
@@ -25,9 +33,15 @@ while True:
 		errorMsg('Error! Should be a number\n')
 
 cipher = ''
+
+#changing plain text to cipher text
 for ltr in txt:
-	ascii = ((ord(ltr) + key) % 90)
-	if ascii < 64:
+	ascii = ord(ltr)
+	if ascii == 32:
+		cipher += ' '
+		continue
+	ascii = ((ascii + key) % 90) #ASCII value of 'Z' = 90
+	if ascii < 64: #ASCII value of 'A' = 65
 		ascii += 64
 	cipher += chr(ascii)
 
